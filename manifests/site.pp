2142385577b58b28d81f04fa729dc896307321a3
mivehk <40775007+mivehk@users.noreply.github.com>
2018-09-16 17:52:40 -0400

    Now master node inherits master server role

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,6 +3,8 @@ file{'/root/README':
 ensure  => file,
 content => ' This is simply a readme file created by kayvan to test file resource under default node ',
 owner   => 'root' ,
-
 }
+}
+node 'puppet2.localdomain' {
+  include role::master_server
 }
