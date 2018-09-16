e593e8282299d2702852d3869812c43350963657
mivehk <40775007+mivehk@users.noreply.github.com>
2018-09-16 17:06:28 -0400

    owner is root

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,8 @@
 node default {
 file{'/root/README':
-ensure => file,
-content => ' This is simply a readme file created by kayvan to test file resource under default node ' ,
+ensure  => file,
+content => ' This is simply a readme file created by kayvan to test file resource under default node ',
+owner   => 'root' ,
+
 }
 }
