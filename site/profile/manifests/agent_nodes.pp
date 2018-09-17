bb429b969621a83291d88b76b9ad19de7911ecc0
mivehk <40775007+mivehk@users.noreply.github.com>
2018-09-17 11:29:54 -0400

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -6,7 +6,7 @@ host{'puppet-agent.localdomain':
 ensure => present,
 ip    => '192.168.18.138',
 }
-host{'centos.localdomain':
+host{'centos-agent.localdomain':
 ensure => present,
 ip    =>'192.168.18.136',
 }
