6606d4f7afe25d587fdf043b267a7d8f00f9e086
mivehk <40775007+mivehk@users.noreply.github.com>
2018-09-17 08:59:54 -0400

    commenting out the README section of default node

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,9 @@
 node default {
-file{'/root/README':
-ensure  => file,
-content => ' This is simply a readme file created by kayvan to test file resource under default node ',
-owner   => 'root' ,
-}
+#file{'/root/README':
+#ensure  => file,
+#content => ' This is simply a readme file created by kayvan to test file resource under default node ',
+#owner   => 'root' ,
+#}
 }
 node 'puppet2.localdomain' {
   include role::master_server
