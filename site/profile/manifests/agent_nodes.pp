1877fa055368c6ab8577a635cc0f2422c035617c
mivehk <40775007+mivehk@users.noreply.github.com>
2018-09-17 11:29:33 -0400

    capability of puppet to include ip of hosts

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,4 +2,12 @@ class profile::agent_nodes{
 include dockeragent
 dockeragent::node {'puppet-agent.localdomain':}
 dockeragent::node {'centos-agent.localdomain':}
+host{'puppet-agent.localdomain':
+ensure => present,
+ip    => '192.168.18.138',
+}
+host{'centos.localdomain':
+ensure => present,
+ip    =>'192.168.18.136',
+}
 }
