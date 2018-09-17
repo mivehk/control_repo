347e1618ba25dad526f6f691f8cb6df4efb1994b
mivehk <40775007+mivehk@users.noreply.github.com>
2018-09-17 11:33:51 -0400

    adding public key of master node

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCzk3UEdIO+yx40g8SX2mMazu6z5/NUUs8vGrj4cqADNOV6uhTYGEO/jXf/K4PdWJ5NgXFyGB/c7BLy6tGao7HUkJq2uWYs0T7yWKkfIEdvHISrr7XCsvocNMMrxYioxyLVyA6SoTnZ4cyda45+FLezY2Wr4lAfCK/A6bUFOQgVkuZsxIuULJOr1OO3+D9pdTiNByMchA+ln6N4/blzW09yDrcQD0N2WiheT7h7GqwZpJ1hp2PzRVyFQNpcoieIu7nD8yRRKWjKEOwRdEagIoxcMN/jp9qJG7ti/Hd7YbNJhi1N1tacW9GSkbx8MwS40ZDf3HHvPc3c4+GqlxSQ5v6r',
 	}  
 }
